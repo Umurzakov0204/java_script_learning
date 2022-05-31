cbaf525530219d116d2976fbc2a28c15ce03aa35
Umurzakov0204 <tomthompson0222@gmail.com>
2022-05-31 23:03:59 +0500

    my second push

---
+++ js_file/main.js
@@ -0,0 +1,20 @@
+const fun = (radius) => {
+    return 4 * 3.14 * radius * radius;
+}
+console.log(fun(5))
+// _____________________________
+const abc = (num) => {
+    if (num > 0) {
+        return num;
+    } else if (num < 0) {
+        return -num;
+    } else {
+        return 0;
+    }
+}
+
+console.log(abc())
+// ______________________________
+
+
+
